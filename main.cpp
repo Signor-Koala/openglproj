88a4f8bca7a73bb32a02fec06d8218cc616815ae
Signor-koala <jeffrey.samuel@protonmail.com>
2022-10-04 11:04:36 +0530

    Clean up

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@
 
 const char *vertexShaderSource = "#version 460 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "out vec4 colour;"
                                  "void main()\n"
                                  "{\n"
+                                 "   colour = vec4(aPos,1.0);"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 460 core\n"
                                    "out vec4 FragColor;\n"
+                                   "in vec4 colour;"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "    FragColor = colour;\n"
                                    "}\0";
 
 int main() {
@@ -23,26 +26,20 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
     GLFWwindow* window = glfwCreateWindow(1280, 720, "i use arch btw", NULL, NULL);
-
     float aspectRatio = 1280.0f/720;
 
     if (window == NULL) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
-
         return -1;
     }
-
     glViewport(0, 0, 1280, 720);
 
     float vertices[] = {
@@ -56,12 +53,6 @@ int main() {
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -70,6 +61,14 @@ int main() {
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+    // Instructions for how to use the data given
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     glCompileShader(fragmentShader);
 
     unsigned int shaderProgram;
@@ -84,18 +83,11 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    bool x = true;
-
     while(!glfwWindowShouldClose(window)) {
-
         //rendering stuff goes here
         glClearColor(0.5f, 0.0f, 0.6f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        if (x) {
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        }
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
